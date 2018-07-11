846ab2dce45613a1f15d6a6108dfb3a2d292e1d6
N16326 <sejin0109@interpark.com>
2018-07-11 09:54:39 +0900

    Scroll Move X, Y

--- app/src/main/java/com/hour24/parallaximageview/MainActivity.java
+++ app/src/main/java/com/hour24/parallaximageview/MainActivity.java
@@ -1,18 +1,36 @@
 package com.hour24.parallaximageview;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int MAX_ALPHA = 225;
+
     private RecyclerView mRecyclerView;
     private ViewAdapter mAdapter;
+    private View mHeader;
+    private View mFix;
+    private View mMove;
 
     private ArrayList<Model> mList;
+
+    int mHeight;
+    float mFixX;
+    float mFixY;
+    float mMoveX;
+    float mMoveY;
+    float mDiffX;
+    float mDiffY;
+    float mTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +40,130 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        mHeader = findViewById(R.id.header);
+        mFix = findViewById(R.id.fix);
+        mMove = findViewById(R.id.move);
+
         mAdapter = new ViewAdapter(this, getList());
         mRecyclerView.setAdapter(mAdapter);
 
+        // 0 > 투명, 1 > 불투명
+        mHeader.setAlpha((float) 0.0);
+        mHeight = getResources().getDimensionPixelOffset(R.dimen.header) * 5; // 스크롤 Y 값과 header 높이 값이 맞지 않음
+
+        ViewTreeObserver.OnGlobalLayoutListener globalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                Log.e("sjjang", "height : " + mHeight);
+
+                mFixX = (float) mFix.getX();
+                mFixY = (float) mFix.getY();
+                Log.e("sjjang", "fixX : " + mFixX);
+                Log.e("sjjang", "fixY : " + mFixY);
+
+                mMoveX = (float) mMove.getX();
+                mMoveY = (float) mMove.getY();
+                Log.e("sjjang", "moveX : " + mMoveX);
+                Log.e("sjjang", "moveY : " + mMoveY);
+
+                mDiffX = (float) Math.abs(mFixX - mMoveX) / mHeight;
+                mDiffY = (float) Math.abs(mFixY - mMoveY) / mHeight;
+                Log.e("sjjang", "diffX : " + mDiffX);
+                Log.e("sjjang", "diffY : " + mDiffY);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    mRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    mRecyclerView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+            }
+        };
+
+        mRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListener);
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                Log.e("sjjang", "y : " + recyclerView.computeVerticalScrollOffset());
+                int y = recyclerView.computeVerticalScrollOffset();
+
+                // header alpha
+                mHeader.setAlpha(getAlpha(y));
+
+//                07-11 09:17:30.152 7202-7202/com.hour24.parallaximageview E/sjjang: height : 1000
+//                07-11 09:17:30.152 7202-7202/com.hour24.parallaximageview E/sjjang: fixX : 1280
+//                07-11 09:17:30.152 7202-7202/com.hour24.parallaximageview E/sjjang: fixY : 40
+//                07-11 09:17:30.152 7202-7202/com.hour24.parallaximageview E/sjjang: moveX : 1120
+//                07-11 09:17:30.152 7202-7202/com.hour24.parallaximageview E/sjjang: moveY : 320
+//                07-11 09:17:30.152 7202-7202/com.hour24.parallaximageview E/sjjang: diffX : 6.25
+//                07-11 09:17:30.152 7202-7202/com.hour24.parallaximageview E/sjjang: diffY : 280.0
+
+                if (y < 1) {
+                    // 최초 위치
+                    mMove.setY(mMoveY);
+                } else if (y >= 1 && y <= mHeight) {
+                    // Moving;
+                    mMove.setY(mMoveY - (mDiffY * y));
+                } else {
+                    // 스크롤 내리고 난 뒤 위치
+                    mMove.setY(mFixY);
+                }
+                mMove.setX(getMoveX(y));
+            }
+        });
     }
+
+    // Get Alpha
+    private float getAlpha(int y) {
+        try {
+            // 헤더 높이 만큼 Alpha 값 구함
+            if (y > 0 && y <= mHeight) {
+                return (float) y / mHeight;
+            }
+
+            // y 값 한번 더 체크
+            if (y < 1) {
+                return (float) 0.0;
+            } else if (y >= mHeight) {
+                return (float) 1.0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return (float) 1.0;
+    }
+
+    // Get Move X Point
+    private float getMoveX(int y) {
+        try {
+            if (y < 1) {
+                // 최초 위치
+                return mMoveX;
+            } else if (y >= 1 && y <= mHeight) {
+                // Moving;
+                return (float) mMoveX + (mDiffX * y);
+            } else {
+                // 스크롤 내리고 난 뒤 위치
+                return mFixX;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mFixX;
+    }
+
+    // Get Move Y Point
+
 
     public ArrayList<Model> getList() {
 
--- app/src/main/java/com/hour24/parallaximageview/Utils.java
+++ app/src/main/java/com/hour24/parallaximageview/Utils.java
@@ -1,5 +1,6 @@
 package com.hour24.parallaximageview;
 
+import android.content.Context;
 import android.databinding.BindingAdapter;
 import android.widget.ImageView;
 
@@ -17,24 +18,32 @@ public class Utils {
     public static void setLoadImage(final ParallaxImageView view, String url) {
         try {
 
-            final ImageView imageView = new ImageView(view.getContext());
-            Picasso.get()
-                    .load(url)
-                    .into(imageView, new Callback() {
-                        @Override
-                        public void onSuccess() {
-                            view.setImageDrawable(imageView.getDrawable());
-                        }
-
-                        @Override
-                        public void onError(Exception e) {
-
-                        }
-                    });
+//            final ImageView imageView = new ImageView(view.getContext());
+//            Picasso.get()
+//                    .load(url)
+//                    .into(imageView, new Callback() {
+//                        @Override
+//                        public void onSuccess() {
+//                            view.setImageDrawable(imageView.getDrawable());
+//                        }
+//
+//                        @Override
+//                        public void onError(Exception e) {
+//
+//                        }
+//                    });
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static float getDPFromPx(final Context context, final float px) {
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float getPXFromDp(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
     }
 
 }
